7f05cfe2903238c6bd487479cbc07a7a024c38f9
Gukhwa Jeon <jeon.gukhwa7@gmail.com>
2020-04-24 17:23:54 +0200

    2. left selected picture to display name, right selected picture to display view

--- 05 PickerViewMisson MultiComponent/ViewController.swift
+++ 05 PickerViewMisson MultiComponent/ViewController.swift
@@ -54,8 +54,12 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        lblImageFileName.text = imageFileName[row]
-        imageView.image = imageArray[row]
+        if (component == 0) {
+            lblImageFileName.text = imageFileName[row]
+        } else {
+            imageView.image = imageArray[row]
+
+        }
     }
 }
 
